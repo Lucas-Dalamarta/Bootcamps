abdc1a9a586aabed2c436c92f4a75839c432e2ec
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2022-05-24 15:24:08 -0300

    feat: Test GPG key

---
+++ GoStack/gobarber/server/estados.js
@@ -0,0 +1,35 @@
+const estados = [
+'AC',
+'AL',
+'AP',
+'AM',
+'BA',
+'CE',
+'DF',
+'ES',
+'GO',
+'MA',
+'MT',
+'MS',
+'MG',
+'PA',
+'PB',
+'PR',
+'PE',
+'PI',
+'RJ',
+'RN',
+'RS',
+'RO',
+'RR',
+'SC',
+'SP',
+'SE',
+'TO'
+]
+const estadoOptions = [];
+
+estados.forEach(estado => estadoOptions.push({ value: estado, label: estado }));
+
+
+console.log(estadoOptions);
